0a17f16e0cd7a265c6338a01c905aac1675b8353
Kennedy Lodonu <ylodonu@gmail.com>
2023-01-04 18:08:38 +0000

    add jasmine testing

--- build/index.js
+++ build/index.js
@@ -1,17 +1,15 @@
+"use strict";
 // import express from 'express';
 // const app = express();
 // const port = 3000;
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // app.get('/api', (req, res) => {
 //   res.send('Hello, world!');
 // });
-
 // app.listen(port, () => {
 //   console.log(`server started at localhost:${port}`);
 // });
-
-
-const myFunc = (num: number): number => {
-  return num * num;
+const myFunc = (num) => {
+    return num * num;
 };
-export default myFunc;
+exports.default = myFunc;
